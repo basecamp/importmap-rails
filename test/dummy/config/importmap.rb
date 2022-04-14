f816d04a44d877e1026efb6d3b63ecac6772023e
removedporn <86824510+removedporn@users.noreply.github.com>
2022-04-14 18:17:58 +0800

    Delete test directory

--- test/dummy/config/importmap.rb
+++
@@ -1,4 +0,0 @@
-pin_all_from "app/assets/javascripts", preload: false
-
-pin "md5", to: "https://cdn.skypack.dev/md5"
-pin "not_there", to: "nowhere.js", preload: false
--- test/dummy/config/routes.rb
+++
@@ -1,3 +0,0 @@
-Rails.application.routes.draw do
-  mount Importmap::Engine => "/importmap"
-end
